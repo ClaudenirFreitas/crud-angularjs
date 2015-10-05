56e72f7cdb41034bdd1560b05fdd3e2713b0a38a
Claudenir Machado <claudenir.machado@sensedia.com>
2015-10-05 19:20:39 -0300

    Insercao edicao

--- controles/GrafostCtrl.js
+++ controles/GrafostCtrl.js
@@ -1,21 +1,39 @@
-var grafos = angular.module('GrafosApp', []); 
-grafos.controller('GrafostCtrl', function ($scope) { // Nome do Controler - GrafosCtrl 
-	$scope.cidades = []; // Vetor esta vazio para receber novas cidades
-	
+var grafos = angular.module('GrafosApp', []);
 
+grafos.controller('GrafostCtrl', function($scope) { // Nome do Controler - GrafosCtrl
+    $scope.cidades = []; // Vetor esta vazio para receber novas cidades
 
+    function Trajeto(id, cidadeOne, cidadeTwo, distancia) {
+        this.id = id;
+        this.cidadeOne = cidadeOne;
+        this.cidadeTwo = cidadeTwo;
+        this.distancia = distancia;
+    }
 
-  $scope.adicionar = function(){ // Função para adicionar as cidades e a distância entre elas. Através do Push
-  	$scope.cidades.push({
-  		cidadeOne:$scope.cidades.cidadeOne,
-  		cidadeTwo:$scope.cidades.cidadeTwo,
-  		distancia:$scope.cidades.distancia + " KM" 
-  	});
-  };
-  
-  $scope.remove = function($index){ 
-  	$scope.cidades.splice($index, 1); 
-  };
+    $scope.salvar = function() { // Função para adicionar as cidades e a distância entre elas. Através do Push
+        if (!$scope.cidade.id)
+            $scope.cidades.push(
+                new Trajeto(
+                    $scope.cidades.length + 1,
+                    $scope.cidade.cidadeOne,
+                    $scope.cidade.cidadeTwo,
+                    $scope.cidade.distancia
+                ));
+        $scope.cidade = {};
+    };
+
+    $scope.editar = function(id) {
+        for (var cidade in $scope.cidades)
+            if ($scope.cidades[cidade].id === id) {
+                $scope.cidade = $scope.cidades[cidade];
+                break;
+            }
+    };
+
+    $scope.remove = function(id) {
+        $scope.cidades = $scope.cidades.filter(function(cidade) {
+            return cidade.id !== id;
+        });
+    };
 
 });
-
